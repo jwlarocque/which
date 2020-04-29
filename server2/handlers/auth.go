f7d8da51d8c9c72b6f4b92847e77e9cbeb8fc78a
jwlarocque <jwlarocque@gmail.com>
2020-04-28 21:54:18 -0600

    Consolidate auth handlers to one file

--- server2/handlers/auth.go
+++ server2/handlers/auth.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"context"
+	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -73,7 +76,110 @@ func (handler *Auth) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 
 // == Login handler ================================
 
+type Login struct {
+	userStore    which.UserStore
+	sessionStore which.SessionStore
+	config       *oauth2.Config
+}
+
+func (handler *Login) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	_, err := sessionFromRequest(req, handler.sessionStore)
+	if err != nil {
+		// no valid session, redirect to oauth
+		state, err := encodeState(state{QueryString: req.URL.Query().Encode(), RandomState: randomString()})
+		if err != nil {
+			log.Printf("Unable to generate random state string: %v\n", err)
+			http.Redirect(resp, req, "/", http.StatusInternalServerError)
+		}
+		addCookie(resp, "state", state, time.Now().Add(time.Minute)) // TODO: abstract out this duration?
+		http.Redirect(resp, req, handler.config.AuthCodeURL(state), http.StatusTemporaryRedirect)
+	} else {
+		// already logged in
+		http.Redirect(resp, req, "/", http.StatusOK)
+	}
+}
+
 // == Callback handler ================================
+
+type Callback struct {
+	userStore    which.UserStore
+	sessionStore which.SessionStore
+	config       *oauth2.Config
+}
+
+// TODO: this function is still rather long
+func (handler *Callback) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	// check that callback and cookie states match
+	s, err := stateFromCallback(req)
+	if err != nil {
+		log.Printf("State cookie/formvalue mismatch, possibly due to CSRF attack: %v\n", err)
+		http.Redirect(resp, req, "/", http.StatusForbidden)
+		return
+	}
+
+	// cookie matches state, proceed
+	user, err := createUserFromCallback(req, handler.config)
+	if err != nil {
+		log.Printf("Unable to create User from request: %v\n", err)
+		http.Redirect(resp, req, "/", http.StatusInternalServerError)
+		return
+	}
+
+	err = handler.userStore.Insert(user)
+	if err != nil {
+		log.Printf("Unable to add User to store: %v\n", err)
+		http.Redirect(resp, req, "/", http.StatusInternalServerError)
+		return
+	}
+
+	session := which.Session{
+		ID:      randomString(),
+		UserID:  user.ID,
+		Created: time.Now(),
+		Expires: time.Now().Add(which.CookieDuration),
+	}
+	err = handler.sessionStore.Insert(session)
+	if err != nil {
+		log.Printf("Unable to add Session to store: %v\n", err)
+		http.Redirect(resp, req, "/", http.StatusInternalServerError)
+		return
+	}
+
+	addCookie(resp, "session", session.ID, session.Expires)
+	http.Redirect(resp, req, "/?"+s.QueryString, http.StatusTemporaryRedirect)
+}
+
+// TODO: passing config down through both of these function is awkward
+func createUserFromCallback(req *http.Request, config *oauth2.Config) (which.User, error) {
+	userData, err := getUserData(req.FormValue("code"), config)
+	if err != nil {
+		return which.User{}, err
+	}
+	var user which.User
+	err = json.Unmarshal(userData, &user)
+	if err != nil {
+		return which.User{}, err
+	}
+	return user, nil
+}
+
+// getUserData exchanges an OAuth code for user data from Google
+func getUserData(authCode string, config *oauth2.Config) ([]byte, error) {
+	token, err := config.Exchange(context.Background(), authCode)
+	if err != nil {
+		return nil, fmt.Errorf("failed to exchange code for token: %s", err.Error())
+	}
+	response, err := http.Get(which.OauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get userinfo from Google Oauth: %s", err.Error())
+	}
+	defer response.Body.Close()
+	data, err := ioutil.ReadAll(response.Body) // TODO: improve this?  why using ioutil.ReadAll?
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %s", err.Error())
+	}
+	return data, nil
+}
 
 // == Status handler ================================
 
@@ -135,4 +241,33 @@ func stateFromCallback(req *http.Request) (state, error) {
 		return state{}, err
 	}
 	return decodeState(stateCookie.Value)
+}
+
+func randomString() string {
+	bytes := make([]byte, 32)
+	_, err := rand.Read(bytes)
+	if err != nil {
+		log.Fatalf("failed to generate random string: %v\n", err)
+	}
+	return base64.URLEncoding.EncodeToString(bytes)
+}
+
+// returns a Session if the request has a valid session cookie, else error
+// TODO: somewhat awkward - retrieves session from store using id from request data
+func sessionFromRequest(req *http.Request, store which.SessionStore) (which.Session, error) {
+	sessionCookie, err := req.Cookie("session")
+	if err != nil {
+		return which.Session{}, fmt.Errorf("failed to get session cookie from request: %v", err)
+	}
+	session, err := store.Fetch(sessionCookie.Value)
+	if err != nil {
+		return which.Session{}, fmt.Errorf("no session matching cookie: %v", err)
+	}
+	if session.ID != sessionCookie.Value {
+		return which.Session{}, fmt.Errorf("session ID from db didn't match cookie (this should be impossible)")
+	}
+	if time.Now().After(session.Expires) {
+		return which.Session{}, fmt.Errorf("session expired")
+	}
+	return session, nil
 }
